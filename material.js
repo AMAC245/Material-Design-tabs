8a28963adce283bf7998f341edac738f004d1db9
Andrew McGillivray <ap.mcgillivray@gmail.com>
2017-10-26 11:25:52 +1100

    ES6 and clarifying var names

--- material.js
+++ material.js
@@ -1,31 +1,31 @@
 'use strict';
 
 //create ripple
-var tabs = document.querySelectorAll('li');
+const tabs = document.querySelectorAll('li');
 
 Array.prototype.forEach.call(tabs, function (b) {
   b.addEventListener('click', createRipple);
 });
 
 function createRipple(e) {
-  var circle = document.createElement('div');
+  const circle = document.createElement('div');
   this.appendChild(circle);
 
-  var d = Math.max(this.clientWidth, this.clientHeight);
+  let d = Math.max(this.clientWidth, this.clientHeight);
 
   circle.style.width = circle.style.height = d + 'px';
 
-  var rect = this.getBoundingClientRect();
-  circle.style.left = e.clientX - rect.left - d / 2 + 'px';
-  circle.style.top = e.clientY - rect.top - d / 2 + 'px';
+  const dimensions = this.getBoundingClientRect();
+  circle.style.left = e.clientX - dimensions.left - d / 2 + 'px';
+  circle.style.top = e.clientY - dimensions.top - d / 2 + 'px';
 
   circle.classList.add('ripple');
 }
 
 //slide to corresponding tab
 $("li").click(function () {
-  var tabreference = $(this).index();
-  var slide = 160 * tabreference;
+  const tabPosition = $(this).index();
+  const slide = 160 * tabPosition;
 
   $(".slider").css({
     left: slide + "px"
